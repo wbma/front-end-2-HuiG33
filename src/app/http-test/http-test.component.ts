779dda3d86438f3f453eed5c981be51a4b9fa170
HuiG33 <joni.tefke@metropolia.fi>
2018-01-19 13:23:12 +0200

    message

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -9,29 +9,34 @@ import {HttpClient} from '@angular/common/http';
 export class HttpTestComponent implements OnInit {
 
   someData: String = 'hello';
-  imgFolder: String = 'http://media.mw.metropolia.fi/wbma/uploads/';
-  imgUrl: String= '';
+  apiurl = 'http://media.mw.metropolia.fi/wbma';
+  mediaurl = 'http://media.mw.metropolia.fi/wbma/uploads/';
+  mediaArray: any;
 
   constructor(private http:HttpClient) { }
 // makes request and subscribes to the response. save it to (data)
   getJSON(){
-    this.http.get('assets/package.json').subscribe((data) => {
+    interface Myinterface {
+      license: string;
+    }
+
+    this.http.get<Myinterface>('assets/package.json').subscribe(data => {
       console.log(data);
-      this.someData = data['license']; //or data.license
+      this.someData = data.license;
     });
   }
 
-  getMedia(){
-    this.http.get('http://media.mw.metropolia.fi/wbma/media').subscribe(data => {
+  getAllMedia(){
+    this.http.get(this.apiurl + '/media').subscribe(data => {
       console.log(data);
-      this.imgUrl = data[0].filename;
+      this.mediaArray = data;
     });
 
   }
 
   ngOnInit() {
     this.getJSON();
-    this.getMedia();
+    this.getAllMedia();
   }
 
 }
